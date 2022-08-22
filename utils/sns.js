b972baf7083d78525f05b83ab4fb0297ed18ac86
mathmartins2 <dev.sincroni@gmail.com>
2022-08-22 00:55:42 -0300

    v2

--- utils/sns.js
+++ utils/sns.js
@@ -4,5 +4,21 @@ import snsAwsConfig  from '../config/snsAwsConfig.js';
 AWS.config.update(snsAwsConfig);
 
 export default function publishSms({ sns, Message, PhoneNumber }) {
-    return sns.publish({Message, PhoneNumber}).promise();
+    try {
+        PhoneNumber = validatePhoneNumber(PhoneNumber);
+
+        return sns.publish({Message, PhoneNumber}).promise();
+    } catch (error) {
+        throw error;
+    }
+}
+
+const validatePhoneNumber = (phoneNumber) => {
+    phoneNumber = phoneNumber.replace(/[^\d]/g, '');
+
+    if(phoneNumber.length !== 13) {
+        throw('Invalid phone number');
+    }
+
+    return phoneNumber;
 }
